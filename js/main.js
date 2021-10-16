4c876919aa815c86f5b35d759f84097a7fbbf807
web-proger04 <92223429+web-proger04@users.noreply.github.com>
2021-10-16 12:49:17 +0500

    адаптация

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const schemeSvg = document.querySelector('.scheme-svg');
-const totalPriceTag = document.querySelector('.price-total')
+const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.header__menu');
+
 let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener('click', (event) => {
@@ -10,3 +13,7 @@ schemeSvg.addEventListener('click', (event) => {
 		totalPriceTag.textContent = totalPrice;
 	}
 });
+menuButton.addEventListener('click', () => {
+	console.log('click yes');
+	menu.classList.toggle("is-open");
+})
